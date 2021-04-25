d86a3b2c6e20eccec93b996b32ccea55b8756683
pritamrao746 <49559586+pritamrao746@users.noreply.github.com>
2021-04-25 12:30:29 +0530

    Add files via upload

---
+++ sample.h
@@ -0,0 +1,19 @@
+// Pritam Rao
+// 2018130044 , 48
+// SPCC
+// Exp 3 - To create user defined library in open source environment and use it for particular functions
+
+#ifndef sample_h__
+#define sample_h__
+extern double eTOpowerx(double);
+extern double aTOpowerx(double,int);
+extern double exponentialGrowth(int,int,int);
+extern double exponentialDecay(int,int,int);
+extern long long exponentiationModulo(int,int,int);
+extern double sinhx(double);
+extern double coshx(double);
+extern double tanhx(double);
+extern double cothx(double);
+extern double cosechx(double);
+
+#endif // sample_h__
